4a0f277ee56c503a249bbe3f14c1a501c4d83558
mohammad karimian <m.karimian1993@gmail.com>
2020-01-31 01:56:45 +0330

    robot-name

---
+++ 009-robot_name/robot-name-other.ts
@@ -0,0 +1,34 @@
+const names: {[key: string]: boolean} = {};
+
+export default class RobotOther {
+    public name: string;
+    constructor() {
+        this.name = this.getName();
+    }
+
+    randLetter(): string {
+        return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[Math.floor(Math.random() * 26)];
+    }
+
+    randNumber(): string {
+        return '0123456789'[Math.floor(Math.random() * 10)];
+    }
+
+    randName(): string {
+        return this.randLetter() + this.randLetter() +
+            this.randNumber() + this.randNumber() + this.randNumber();
+    }
+
+    getName(): string {
+        let newName = this.randName();
+        while (names[newName] === true) {
+            newName = this.randName();
+        }
+        names[newName] = true;
+        return newName;
+    }
+
+    resetName(): void {
+        this.name = this.getName();
+    }
+}
